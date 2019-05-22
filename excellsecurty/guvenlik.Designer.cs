12de96db51c9b34730f2f83169fd9c0b17f2b238
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2019-05-22 06:08:33 +0300

    Bekra Excell Virüs Tarama Programı
    Bu program ile excel dosyalarında zararlı kodları tespit edebilirsiniz.

---
+++ excellsecurty/guvenlik.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace excellsecurty
+{
+    partial class guvenlik
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(guvenlik));
+            this.buttonX1 = new DevComponents.DotNetBar.ButtonX();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.ribbonControl1 = new DevComponents.DotNetBar.RibbonControl();
+            this.groupPanel1 = new DevComponents.DotNetBar.Controls.GroupPanel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonX2 = new DevComponents.DotNetBar.ButtonX();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxX1 = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonX1
+            // 
+            this.buttonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX1.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX1.Location = new System.Drawing.Point(42, 83);
+            this.buttonX1.Name = "buttonX1";
+            this.buttonX1.Size = new System.Drawing.Size(101, 41);
+            this.buttonX1.TabIndex = 0;
+            this.buttonX1.Text = "Open File";
+            this.buttonX1.Click += new System.EventHandler(this.buttonX1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // ribbonControl1
+            // 
+            this.ribbonControl1.CaptionVisible = true;
+            this.ribbonControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ribbonControl1.KeyTipsFont = new System.Drawing.Font("Tahoma", 7F);
+            this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
+            this.ribbonControl1.Name = "ribbonControl1";
+            this.ribbonControl1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 2);
+            this.ribbonControl1.Size = new System.Drawing.Size(897, 582);
+            this.ribbonControl1.Style = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
+            this.ribbonControl1.TabGroupHeight = 14;
+            this.ribbonControl1.TabIndex = 1;
+            this.ribbonControl1.Text = "ribbonControl1";
+            // 
+            // groupPanel1
+            // 
+            this.groupPanel1.CanvasColor = System.Drawing.SystemColors.Control;
+            this.groupPanel1.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
+            this.groupPanel1.Controls.Add(this.pictureBox2);
+            this.groupPanel1.Controls.Add(this.label3);
+            this.groupPanel1.Controls.Add(this.buttonX2);
+            this.groupPanel1.Controls.Add(this.label2);
+            this.groupPanel1.Controls.Add(this.richTextBox1);
+            this.groupPanel1.Controls.Add(this.label1);
+            this.groupPanel1.Controls.Add(this.textBoxX1);
+            this.groupPanel1.Controls.Add(this.buttonX1);
+            this.groupPanel1.Location = new System.Drawing.Point(0, 25);
+            this.groupPanel1.Name = "groupPanel1";
+            this.groupPanel1.Size = new System.Drawing.Size(907, 435);
+            // 
+            // 
+            // 
+            this.groupPanel1.Style.BackColor2SchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground2;
+            this.groupPanel1.Style.BackColorGradientAngle = 90;
+            this.groupPanel1.Style.BackColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground;
+            this.groupPanel1.Style.BorderBottom = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.groupPanel1.Style.BorderBottomWidth = 1;
+            this.groupPanel1.Style.BorderColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.groupPanel1.Style.BorderLeft = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.groupPanel1.Style.BorderLeftWidth = 1;
+            this.groupPanel1.Style.BorderRight = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.groupPanel1.Style.BorderRightWidth = 1;
+            this.groupPanel1.Style.BorderTop = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.groupPanel1.Style.BorderTopWidth = 1;
+            this.groupPanel1.Style.CornerDiameter = 4;
+            this.groupPanel1.Style.CornerType = DevComponents.DotNetBar.eCornerType.Rounded;
+            this.groupPanel1.Style.TextAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Center;
+            this.groupPanel1.Style.TextColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText;
+            this.groupPanel1.Style.TextLineAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Near;
+            this.groupPanel1.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(3, 130);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(375, 279);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(275, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Analyze";
+            // 
+            // buttonX2
+            // 
+            this.buttonX2.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX2.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX2.Location = new System.Drawing.Point(246, 72);
+            this.buttonX2.Name = "buttonX2";
+            this.buttonX2.Size = new System.Drawing.Size(128, 52);
+            this.buttonX2.TabIndex = 5;
+            this.buttonX2.Text = "Start analyzing";
+            this.buttonX2.Click += new System.EventHandler(this.buttonX2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(596, -3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Analiz Sonucu";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.Location = new System.Drawing.Point(384, 16);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(498, 393);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(31, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File Path to Analysis";
+            // 
+            // textBoxX1
+            // 
+            // 
+            // 
+            // 
+            this.textBoxX1.Border.Class = "TextBoxBorder";
+            this.textBoxX1.Location = new System.Drawing.Point(37, 53);
+            this.textBoxX1.Name = "textBoxX1";
+            this.textBoxX1.Size = new System.Drawing.Size(118, 20);
+            this.textBoxX1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(41, 27);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // guvenlik
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 449);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupPanel1);
+            this.Controls.Add(this.ribbonControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "guvenlik";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.groupPanel1.ResumeLayout(false);
+            this.groupPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.ButtonX buttonX1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private DevComponents.DotNetBar.RibbonControl ribbonControl1;
+        private DevComponents.DotNetBar.Controls.GroupPanel groupPanel1;
+        private System.Windows.Forms.Label label1;
+        private DevComponents.DotNetBar.Controls.TextBoxX textBoxX1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private DevComponents.DotNetBar.ButtonX buttonX2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
---
+++ excellsecurty/guvenlik.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Windows.Forms;
+using Ionic.Zip;
+
+namespace excellsecurty
+{
+    public partial class guvenlik : Form
+    {
+        StreamReader sr;
+        string cikartilacakyol= Application.StartupPath + @"\analiz";
+        private string bekra = "Bekra Hayr Nester";
+        public guvenlik()
+        {
+            InitializeComponent();
+        }
+        private void buttonX1_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK==openFileDialog1.ShowDialog())
+            {
+                textBoxX1.Text = openFileDialog1.FileName.ToString();
+                MessageBox.Show("Dosya Seçildi",bekra);
+            }
+        }
+        private void buttonX2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                zipcikar();
+                kontrol();
+                Directory.Delete(cikartilacakyol, true);
+            }
+            catch (Exception)
+            {
+                Directory.Delete(cikartilacakyol, true);
+            }
+        }
+        void zipcikar()
+        {
+            ZipFile zipFile = new ZipFile(textBoxX1.Text);
+            zipFile.ExtractAll(cikartilacakyol);
+        }
+        void kontrol()
+        {
+            //Directory.GetFileSystemEntries(cikartilacakyol)[0].ToString().Substring(cikartilacakyol.Length);
+            var dz = Directory.GetDirectories(cikartilacakyol+@"\xl");
+            for (int i = 0; i < dz.Length; i++)
+            {
+                string vrtlstring= dz[i].ToString().Substring(cikartilacakyol.Length).Replace("\\xl\\","").Trim();
+                if (vrtlstring== "externalLinks")
+                {
+                    kontrolxml();
+                    break;
+                }
+                else
+                {
+                    if (dz.Length-1==i)
+                    {
+                        richTextBox1.Text = "Excel'de Virüs Yok :)";
+                    }
+                }
+            }
+        }
+        void kontrolxml()
+        {                 //C:\Users\Ebubekir\source\repos\excellsecurty\excellsecurty\bin\Debug\analiz\xl\externalLinks
+            string vrtlyol = cikartilacakyol + @"\xl\externalLinks\externalLink1.xml";
+            ArrayList vrtlary = new ArrayList();
+            try
+            {
+                using (sr = File.OpenText(vrtlyol))
+                {
+                    string telefon = sr.ReadLine();
+                    while (telefon != null)
+                    {
+                        //sycc++;
+                        vrtlary.Add(telefon);
+                        telefon = sr.ReadLine();
+                    }
+                }
+                richTextBox1.Text = "                                        !!! Warning Virus !!!";
+                string s = "=\"";
+                string s1 = "\"><ddeItems><ddeItem name=\"_xlbgnm.A1\" advise=\"1\"/><ddeItem name=\"StdDocumentName" +
+    "\" ole=\"1\" advise=\"1\"/></ddeItems></ddeLink></externalLink>";
+                richTextBox1.Text += "\r"; richTextBox1.Text += "\r";
+                richTextBox1.Text += vrtlary[1].ToString().Substring(166, 16).Replace(s," = ");
+                richTextBox1.Text += "\r";
+                richTextBox1.Text += vrtlary[1].ToString().Substring(184).Replace(s1," ").Replace(s," = ");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
+
+
